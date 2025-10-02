cfce9fd2366d1f705db4455cd11914587c629f95
izzy-u <isabellayoeu@gmail.com>
2025-10-02 10:14:40 -0700

    new base paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  output: 'export',
+  basePath: '/izzy-u-site',
+  assetPrefix: '/izzy-u-site/',
 };
 
 export default nextConfig;
