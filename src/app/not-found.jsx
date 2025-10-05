8a4205255e9288cd3cabf564a89215938f713196
izzy-u <isabellayoeu@gmail.com>
2025-10-04 19:33:43 -0700

    new path for the not-found patch

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,13 +6,13 @@ export default function Custom404() {
     <>
      <div className = "relative min-h-screen">
         <div className = "fixed inset-0 -z-10"> 
-     <CablesPatch patchDir={"/izzy-u-site/notfound-patch/"} canvasId={"othercanvas"} patchOptions={{ "glCanvasResizeToWindow": true }}/>
+     <CablesPatch patchDir={"/notfound-patch/"} canvasId={"othercanvas"} patchOptions={{ "glCanvasResizeToWindow": true }}/>
      </div>
       <div className = "container mx-auto max-w-5xl justify-center align-center pointer-events-auto">
     <div className = "bg-card p-8 rounded-lg shadow-xs border border-gray-200">
         <h1 className = "text-glow font-semibold mb-3 justify-center align-center courier-font text-blue-400 animate-pulse-subtle"> whoops!</h1>
         <h3 className = "text-2xl text-glow font-semibold mt-2 align-center courier-font text-bg-primary mb-5"> i think you're lost. </h3>
-        <a href="/izzy-u-site/" className = "text-sm text-glow p-2 m-1 rounded-full border border-blue-400 text-blue-400 hover:bg-primary/10 hover:text-purple-300 hover:border-purple-300 transition-colors duration-300 courier-font"> let's go home. </a>
+        <a href="/" className = "text-sm text-glow p-2 m-1 rounded-full border border-blue-400 text-blue-400 hover:bg-primary/10 hover:text-purple-300 hover:border-purple-300 transition-colors duration-300 courier-font"> let's go home. </a>
 
         </div>
         </div>
